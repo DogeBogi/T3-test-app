ac0d6a6f3802023add50fc1b96a014863df20408
DogeBogi <sobakichkusakich@gmail.com>
2025-03-21 10:40:20 +0300

    Image next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
 import { db } from "~/server/db";
 import { getMyImages } from "~/server/queries";
-
 export const dynamic = "force-dynamic"
-
 
 async function Images (){
   const images = await getMyImages();
   return (
     <div className="flex flex-row flex-wrap gap-4">
     {images.length === 0 && <h1 className="mr-auto ml-auto text-4xl ">Send your images</h1>}
-    {images.map((image) => (<div key={image.id} className="w-48 flex flex-col"> 
-      <img src={image.url} alt="image"  /> 
+    {images.map((image) => (<div key={image.id} className="w-48 h-48 flex flex-col"> 
+      <Image src={image.url} alt={image.name} width={192} height={192} /> 
       <div>
         {image.name}
       </div>
