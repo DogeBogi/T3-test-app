1f5c87b3d5d9ac8bdd4be794f456641b21597bd3
DogeBogi <sobakichkusakich@gmail.com>
2025-03-23 14:10:29 +0300

    make completly toasts shad

--- src/app/_components/simpleuploadbutton.tsx
+++ src/app/_components/simpleuploadbutton.tsx
@@ -4,6 +4,7 @@ import { UploadButton } from '~/utils/uploadthing'
 import { useRouter } from 'next/navigation'
 import { useUploadThing } from "~/utils/uploadthing";
 import { toast } from "sonner"
+import { useEffect, useState } from 'react';
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -39,18 +40,91 @@ function UploadSVG(){
 
   )
 }
+function LoadingSpinnerSVG() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="white"
+    >
+      <path
+        d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+        opacity=".25"
+      />
+      <path
+        d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+        className="spinner_ajPY"
+      />
+    </svg>
+  );
+}
+
+
+const CorrectAnswerMarker = () => {
+  
+
+  return (
+    <svg
+    style={{display: 'inline-block'}}
+    width="24"
+    height="24"
+    viewBox="0 0 50 50"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    stroke="white"
+    strokeWidth="5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <style>
+      {`
+        @keyframes draw {
+          from {
+            stroke-dasharray: 50;
+            stroke-dashoffset: 50;
+          }
+          to {
+            stroke-dasharray: 50;
+            stroke-dashoffset: 0;
+          }
+        }
+        .checkmark {
+          animation: draw 0.5s ease-out 0.2s forwards;
+        }
+      `}
+    </style>
+    <polyline
+      className="checkmark"
+      points="10,25 20,35 40,10"
+      strokeDasharray="50"
+      strokeDashoffset="50"
+    />
+  </svg>
+  );
+};
+
 
 export default function SimpleUploadButton(){
   const {inputProps} = useUploadThingInputProps("imageUploader",{
     onUploadBegin(){
-      toast("Uploading...", {
+      toast(<div className="flex items-center gap-2 text-white">
+        <LoadingSpinnerSVG /> <span className="text-lg">Uploading...</span>
+      </div>, {
+        duration: 3000,
         id: "upload-begin"
       })
     },
     onClientUploadComplete(){ 
-      router.refresh();
       toast.dismiss("upload-begin")
-      toast("Success Upload")
+      toast(<div className='flex items-center gap-2 text-white'>
+          <CorrectAnswerMarker />
+        <span className='text-lg'>Upload Complete</span>
+      </div>,{
+        id: 'upload-complete'
+      })
+      router.refresh();
     }
   })
   const router = useRouter()
