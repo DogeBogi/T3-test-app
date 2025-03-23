9482bcd2088b472ccc0b2de830fe12d0428b2d92
DogeBogi <sobakichkusakich@gmail.com>
2025-03-23 12:46:20 +0300

    instasll shad ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body className="">
+      <body className={``}>
       
     <NextSSRPlugin
           /**
